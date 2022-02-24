4125596d1417eb2a2ee57015b96bc8d66a9c3f41
fachriansyahmni <fachriansyahmni@gmail.com>
2022-02-24 19:51:26 +0700

    [Completed] Challenges drawing basic

---
+++ drawing_basic/Readme.md
@@ -0,0 +1,11 @@
+# Ruangguru Challenges
+
+**Drawing Basic** 
+<p>with p5js</p>
+
+
+**Result** \
+![Hasil](result.png)
+
+![Challenge simple snowman](Challenge-simple-snowman.png)
+![Challenge waving snowman](Challenge-waving-snowman.png)
---
+++ drawing_basic/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+    createCanvas(1400, 1400);
+}
+
+function draw() {
+    //among us
+    // ellipse(290, 200, 200, 200)
+    // ellipse(120, 350, 80, 100)
+    // ellipse(270, 350, 80, 100)
+    // ellipse(200, 200, 300, 350)
+    // ellipse(200, 170, 200, 150)
+
+    //snowman
+    ellipse(200, 700, 150, 150);
+    ellipse(200, 600, 100, 100);
+    ellipse(200, 540, 80, 80);
+
+    //spongebob (?)
+    ellipse(200, 200, 250, 300);
+    ellipse(200, 250, 182, 60);
+    ellipse(150, 150, 30, 30);
+    ellipse(250, 150, 30, 30);
+    rect(175, 345, 50, 50);
+
+
+    //Camp Id : BE2000921
+    //Nama : Fachriansyah Muh Nur I
+
+    rect(0, 1099, 399, 100);
+    ellipse(200, 1100, 150, 150);
+    ellipse(200, 1000, 100, 100);
+    ellipse(200, 920, 75, 75);
+    line(160, 1000, 100, 920);
+    line(240, 1000, 300, 920);
+}
